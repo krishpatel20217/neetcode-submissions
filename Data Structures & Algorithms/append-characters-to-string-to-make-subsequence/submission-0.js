fcddff355d2be3a45d4f586970c0c0e360bc5de8
krish patel <krishpatel20217@gmail.com>
2026-08-04 21:43:24 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s, t) {
+        
+        let i=0;
+        let j=0;
+        while(i<t.length && j<s.length){
+            if(t[i]===s[j]){
+                i++
+            }
+            j++
+        }
+        return t.length-i;
+    }
+}
