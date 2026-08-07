23d210a64fadbad531595a959299793fb3cba327
krish patel <krishpatel20217@gmail.com>
2026-08-07 19:49:54 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let ans=0;
+        let count=0;
+        for(let i of nums){
+            if(i===1){
+                count++;
+                ans = Math.max(ans, count);
+            }
+            else{ 
+                count=0;
+            }
+        }
+        return ans;
+    }
+}
