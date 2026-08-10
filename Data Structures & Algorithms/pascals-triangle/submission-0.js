39b2075dca497702b0f60a2d0a24c7937ca8e8b7
krish patel <krishpatel20217@gmail.com>
2026-08-10 21:00:33 +0530

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number} numRows
+     * @return {number[][]}
+     */
+    generate(numRows) {
+        const ans = [];
+
+        for (let i = 0; i < numRows; i++) {
+
+            const row = new Array(i + 1).fill(1);
+
+            for (let j = 1; j < i; j++) {
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+
+            ans.push(row);
+        }
+
+        return ans;
+    }
+}
