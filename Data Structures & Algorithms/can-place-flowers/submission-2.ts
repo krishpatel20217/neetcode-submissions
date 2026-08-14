d56c2c54aadef4f04c04d0de1aae0197ed2710f8
krish patel <krishpatel20217@gmail.com>
2026-08-14 13:12:12 +0530

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed: number[], n: number): boolean {
+         if(n===0){
+            return true
+        }
+        for(let i=0;i<flowerbed.length;i++){
+            let left = (i===0) || (flowerbed[i-1]===0)
+            let right = (i === flowerbed.length - 1) || (flowerbed[i+1]===0)
+            if(flowerbed[i]===0 && left && right){
+                flowerbed[i]=1
+               n-- 
+            }
+            if(n===0){
+                return true
+            }
+        }
+        return false
+    }
+}
