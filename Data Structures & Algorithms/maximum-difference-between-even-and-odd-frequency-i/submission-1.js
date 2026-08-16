bdc86f27be6b0d4c4ab012547fcf7f08f7a28c01
krish patel <krishpatel20217@gmail.com>
2026-08-16 19:28:06 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-1

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    maxDifference(s) {
+        let map= new Map();
+        for(let i=0; i<s.length;i++){
+            map.set(s[i], (map.get(s[i]) || 0) + 1);
+        }
+        let maxOdd = 0;
+        let minEven = Infinity;
+
+        for (const [key, value] of map.entries()) {
+            if (value % 2 !== 0) {
+                maxOdd = Math.max(maxOdd, value);
+            } else {
+                minEven = Math.min(minEven, value);
+            }
+        }
+        return maxOdd-minEven
+    }
+}
