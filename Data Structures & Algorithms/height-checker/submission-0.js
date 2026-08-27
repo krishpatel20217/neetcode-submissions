ec8215e9b6da1bccc3144da8bb796aae16aff6aa
krish patel <krishpatel20217@gmail.com>
2026-08-27 09:18:27 +0530

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    heightChecker(heights) {
+        const expected = [...heights].sort((a, b) => a - b);
+
+        let count = 0;
+
+        for (let i = 0; i < heights.length; i++) {
+            if (heights[i] !== expected[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
