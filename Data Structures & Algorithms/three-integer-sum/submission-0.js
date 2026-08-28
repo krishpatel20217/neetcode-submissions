51062e272855ede16e0a942209a543bae692cd2d
krish patel <krishpatel20217@gmail.com>
2026-08-28 10:19:33 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let result = [];
+
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first numbers
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                continue;
+            }
+
+            let left = i + 1;
+            let right = nums.length - 1;
+
+            while (left < right) {
+                let sum = nums[i] + nums[left] + nums[right];
+
+                if (sum === 0) {
+                    result.push([nums[i], nums[left], nums[right]]);
+
+                    left++;
+                    right--;
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right && nums[right] === nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
