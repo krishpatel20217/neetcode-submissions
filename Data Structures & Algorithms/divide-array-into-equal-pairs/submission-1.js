ad4c8e162fd37207df4d9ccf247362f11e09fd3e
krish patel <krishpatel20217@gmail.com>
2026-08-31 11:40:17 +0530

    Add: divide-array-into-equal-pairs - submission-1

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    divideArray(nums) {
+        let map = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+        }
+
+        for (const value of map.values()) {
+            if (value % 2 !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
