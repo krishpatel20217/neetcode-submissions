e9f5ebb63da70983856128874323ad073b1b1e6b
krish patel <krishpatel20217@gmail.com>
2026-09-01 19:04:06 +0530

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    numIdenticalPairs(nums) {
+        let count =0;
+        for(let i=0;i<nums.length;i++){
+            for(let j=i+1;j<nums.length;j++){
+                if(nums[i]===nums[j]){
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+}
