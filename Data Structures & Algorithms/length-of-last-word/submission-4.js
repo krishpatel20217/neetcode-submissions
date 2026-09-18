1ea36950b921df6a414edb8dc9310f270752bdb1
krish patel <krishpatel20217@gmail.com>
2026-09-18 20:40:47 +0530

    Add: length-of-last-word - submission-4

---
+++ Data Structures & Algorithms/length-of-last-word/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLastWord(s) {
+        let count=0;
+        let se=s.trim();
+        let i=se.length-1;
+        while(i>=0){
+        if (se[i] === " ") {
+            return count;
+        }
+
+        count++;
+        i--;
+        }
+        return count
+    }
+}
