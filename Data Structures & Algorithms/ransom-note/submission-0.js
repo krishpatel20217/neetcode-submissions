8b0c36b3dd73626e54d49f5744da34171594a42a
krish patel <krishpatel20217@gmail.com>
2026-09-20 10:29:26 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} ransomNote
+     * @param {string} magazine
+     * @return {boolean}
+     */
+    canConstruct(ransomNote, magazine) {
+        let map = new Map();
+        for(let i=0;i<magazine.length;i++){
+            map.set(magazine[i],(map.get(magazine[i])||0)+1);
+        }
+        for(let i=0;i<ransomNote.length;i++){
+            if(map.has(ransomNote[i]) && (map.get(ransomNote[i]))>0){
+                map.set(ransomNote[i],(map.get(ransomNote[i]))-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
